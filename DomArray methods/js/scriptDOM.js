6020813c1c7a4f7f77ce5b54627335e51744b6f3
Amita999 <amitajethani@gmail.com>
2021-04-07 22:02:03 +0530

    All array methods done

--- DomArray methods/js/scriptDOM.js
+++ DomArray methods/js/scriptDOM.js
@@ -19,12 +19,67 @@ async function getRandomUserFunction(){
    const user = data.results[0];
    const newUser={
        name:`${user.name.first} ${user.name.last}`,
-       money : Math.floor(Math.random() * 1000000 )
+       money : Math.floor(Math.random() * 100000 )
    }
 
    addDataFunction(newUser);
+
 }
 
 function addDataFunction(obj){
     data.push(obj)
+    updateDOMFunction();
 }
+
+function  updateDOMFunction(pdata = data){
+//Clear the main division
+mainEl.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+pdata.forEach(item =>{
+const people = document.createElement('div');
+people.classList.add('person');
+people.innerHTML=`<strong>${item.name}</strong> ${formatMoneyFunction(item.money)}`
+;
+mainEl.appendChild(people);
+})
+}
+
+//Format money
+function formatMoneyFunction(number){
+      return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+function doubleMoneyFunction(){
+     data = data.map((user )=> {
+        return { ...user, money:user.money * 2}}
+        );
+        updateDOMFunction();
+}
+
+function sortByWealthFunction(){
+    data.sort((a , b) => b.money - a.money);
+
+    updateDOMFunction();
+}
+
+function filterMillionaireFunction(){
+    data = data.filter((user) => user.money > 10000000);
+    updateDOMFunction();
+}
+
+function calTotalFunction(){
+    const wealth = data.reduce((acc,user)=>
+    (acc + user.money),0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoneyFunction(wealth)}</strong></h3>`
+    mainEl.appendChild(wealthEl);
+    // console.log(wealth);
+}
+ 
+
+//Event Listeners
+addUserBtn.addEventListener('click',getRandomUserFunction);
+doubleBtn.addEventListener('click',doubleMoneyFunction);
+sortBtn.addEventListener('click',sortByWealthFunction);
+showMnBtn.addEventListener('click',filterMillionaireFunction);
+calculateBtn.addEventListener('click',calTotalFunction)
