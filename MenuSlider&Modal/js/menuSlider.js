3ce1df8e2fe5ea89c8bdb8ceb8ce8aba21753a35
Amita999 <amitajethani@gmail.com>
2021-04-08 18:24:05 +0530

    modal toggle complete

---
+++ MenuSlider&Modal/js/menuSlider.js
@@ -0,0 +1,22 @@
+const toggle = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+const openBtn = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+//Toggle nav
+toggle.addEventListener('click',() =>
+    document.body.classList.toggle('show-nav')
+
+
+); 
+
+//Show Modal
+openBtn.addEventListener('click',()=>
+modal.classList.add('show-modal')
+);
+
+//Hide Modal
+closeBtn.addEventListener('click',()=>
+modal.classList.remove('show-modal'));
+
+window.addEventListener('click',e => e.target ==modal ? modal.classList.remove('show-modal') : false);
